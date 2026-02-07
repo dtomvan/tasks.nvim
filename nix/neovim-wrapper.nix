8c12d33c1aec24d6dc0f6110eb2eaa31e99d1a8f
Tom van Dijk <18gatenmaker6@gmail.com>
2026-02-07 22:47:16 +0100

    ci: don't fixup in the neovim wrapper
    Takes more time than it does anything

--- nix/neovim-wrapper.nix
+++ nix/neovim-wrapper.nix
@@ -3,49 +3,51 @@
   perSystem =
     { pkgs, self', ... }:
     {
-      packages.default = pkgs.wrapNeovimUnstable pkgs.neovim-unwrapped {
-        luaRcContent =
-          #lua
-          ''
-            require'telescope'.setup { extensions = { tasks = {} } }
-            require('telescope').load_extension('tasks')
-            require'tasks'.setup { add_commands = true }
-            local cmp = require'cmp'
-            cmp.setup {
-              sources = { { name = 'tasks' } },
-              mapping = cmp.mapping.preset.insert({
-                  ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-                  ['<C-f>'] = cmp.mapping.scroll_docs(4),
-                  ['<C-Space>'] = cmp.mapping.complete(),
-                  ['<C-e>'] = cmp.mapping.abort(),
-                  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-              }),
-              snippet = {
-                expand = function(args)
-                  vim.snippet.expand(args.body)
-                end,
-              },
-            }
+      packages.default =
+        (pkgs.wrapNeovimUnstable pkgs.neovim-unwrapped {
+          luaRcContent =
+            #lua
+            ''
+              require'telescope'.setup { extensions = { tasks = {} } }
+              require('telescope').load_extension('tasks')
+              require'tasks'.setup { add_commands = true }
+              local cmp = require'cmp'
+              cmp.setup {
+                sources = { { name = 'tasks' } },
+                mapping = cmp.mapping.preset.insert({
+                    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+                    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+                    ['<C-Space>'] = cmp.mapping.complete(),
+                    ['<C-e>'] = cmp.mapping.abort(),
+                    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+                }),
+                snippet = {
+                  expand = function(args)
+                    vim.snippet.expand(args.body)
+                  end,
+                },
+              }
 
-            vim.keymap.set("n", "<leader>tg", require'tasks'.go_to)
-            vim.keymap.set("n", "<leader>tn", require'tasks'.create_from_todo)
-            vim.keymap.set("n", "<leader>tc", require'tasks'.new)
-            vim.keymap.set("n", "<leader>tl", require'tasks'.list)
-            vim.keymap.set("n", "<leader>tq", require'tasks'.qf_list)
-            vim.keymap.set("n", "<leader>to", "<cmd>Telescope tasks<cr>")
-            vim.keymap.set("n", "<leader>tb", "<cmd>Telescope tasks backlinks<cr>")
-          '';
-        wrapperArgs = [
-          "--set"
-          "NVIM_APPNAME"
-          "nvim-tasks.nvim-dev"
-        ];
-        plugins = with pkgs.vimPlugins; [
-          # optional dependencies
-          telescope-nvim
-          nvim-cmp
-          self'.packages.plugin
-        ];
-      };
+              vim.keymap.set("n", "<leader>tg", require'tasks'.go_to)
+              vim.keymap.set("n", "<leader>tn", require'tasks'.create_from_todo)
+              vim.keymap.set("n", "<leader>tc", require'tasks'.new)
+              vim.keymap.set("n", "<leader>tl", require'tasks'.list)
+              vim.keymap.set("n", "<leader>tq", require'tasks'.qf_list)
+              vim.keymap.set("n", "<leader>to", "<cmd>Telescope tasks<cr>")
+              vim.keymap.set("n", "<leader>tb", "<cmd>Telescope tasks backlinks<cr>")
+            '';
+          wrapperArgs = [
+            "--set"
+            "NVIM_APPNAME"
+            "nvim-tasks.nvim-dev"
+          ];
+          plugins = with pkgs.vimPlugins; [
+            # optional dependencies
+            telescope-nvim
+            nvim-cmp
+            self'.packages.plugin
+          ];
+        }).overrideAttrs
+          { dontFixup = true; };
     };
 }
