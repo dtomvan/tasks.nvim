2f53e6259f67c7e876ac11b784e474a67ea37dc8
Tom van Dijk <18gatenmaker6@gmail.com>
2026-02-08 01:50:58 +0100

    ci: disable `loadCheck` check
    double work

--- nix/github-actions.nix
+++ nix/github-actions.nix
@@ -1,6 +1,8 @@
 { inputs, self, ... }:
 {
   flake.githubActions = inputs.nix-github-actions.lib.mkGithubMatrix {
-    checks = inputs.nixpkgs.lib.getAttrs [ "x86_64-linux" ] self.checks;
+    checks = inputs.nixpkgs.lib.genAttrs [ "x86_64-linux" ] (system: {
+      inherit (self.checks.${system}) treefmt integration;
+    });
   };
 }
