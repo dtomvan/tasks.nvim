466e1a8d6327bfb4f2c55f259fe249d9db401b68
Tom van Dijk <18gatenmaker6@gmail.com>
2026-05-09 19:15:47 +0200

    refactor: get rid of import-tree :tada:
    thanks matrix

--- flake.nix
+++ flake.nix
@@ -4,7 +4,6 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
-    import-tree.url = "github:vic/import-tree";
     systems.url = "github:nix-systems/default";
 
     treefmt-nix = {
@@ -19,5 +18,47 @@
   };
 
   outputs =
-    inputs@{ flake-parts, ... }: flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./nix);
+    inputs@{ flake-parts, ... }:
+    let
+      inherit (builtins)
+        readDir
+        attrNames
+        concatMap
+        ;
+
+      inherit (inputs.nixpkgs) lib;
+
+      listFilesRecursiveCond =
+        dir: condition:
+        let
+          internalFunc =
+            folder:
+            let
+              contents = readDir folder;
+            in
+            concatMap (
+              filename:
+              let
+                subpath = folder + "/${filename}";
+                type = contents.${filename};
+              in
+              if condition { inherit filename type; } then
+                if type == "regular" then
+                  [ subpath ]
+                else if type == "directory" then
+                  internalFunc subpath
+                else
+                  [ ]
+              else
+                [ ]
+            ) (attrNames contents);
+        in
+        internalFunc dir;
+
+      isNixFile =
+        { filename, type }:
+        (lib.hasSuffix ".nix" filename || type == "directory") && !lib.hasPrefix "_" filename;
+      import-tree = dir: (listFilesRecursiveCond dir isNixFile);
+    in
+    flake-parts.lib.mkFlake { inherit inputs; } { imports = import-tree ./nix; };
 }
