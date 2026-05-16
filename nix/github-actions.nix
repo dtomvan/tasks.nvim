6eb536190cfc9be1f071754066d1b7f133579177
Tom Oostveen <tomoostveen.06@gmail.com>
2026-05-16 17:53:48 +0200

    chore(.github): migrate to forgejo

--- nix/github-actions.nix
+++
@@ -1,8 +0,0 @@
-{ inputs, self, ... }:
-{
-  flake.githubActions = inputs.nix-github-actions.lib.mkGithubMatrix {
-    checks = inputs.nixpkgs.lib.genAttrs [ "x86_64-linux" ] (system: {
-      inherit (self.checks.${system}) treefmt integration;
-    });
-  };
-}
