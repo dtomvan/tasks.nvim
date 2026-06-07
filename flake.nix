f8c6ea789bb9e9c585e1ba2c46d7cdfc0831da02
Tom Oostveen <tomoostveen.06@gmail.com>
2026-06-07 14:24:26 +0200

    chore: nix flake update

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
-    systems.url = "github:nix-systems/default";
+    systems.url = "github:nix-systems/default/future-26.11";
 
     treefmt-nix = {
       url = "github:numtide/treefmt-nix";
